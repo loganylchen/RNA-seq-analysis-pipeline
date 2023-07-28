5f497b0846db847c1dba00df46de04b1ab4f80d8
loganylchen <yuelong.chen.btr@gmail.com>
2023-07-28 13:02:12 +0800

    color of water mark change to #7fcdbb

--- workflow/scripts/defaultfunction.R
+++ workflow/scripts/defaultfunction.R
@@ -8,7 +8,7 @@
  cex <- convertUnit(unit(1,"npc"), "mm", val=TRUE) /
    convertUnit(unit(1,"grobwidth", textGrob(x$val)), "mm",val=TRUE)
 
- grid.text(x$lab,  rot=rot, gp=gpar(cex = cex, col="white",
+ grid.text(x$lab,  rot=rot, gp=gpar(cex = cex, col="#7fcdbb",
                                         fontface = "bold", alpha = 0.5))
 
  }
