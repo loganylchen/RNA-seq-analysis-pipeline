dfaf1ea74856890e2b12eb8e4a988e56d47bdf0f
logan <yuelong.chen.btr@gmail.com>
2025-10-16 14:30:32 +0000

    fix: refactor fetchsra script to improve readability and streamline symlink creation

--- workflow/scripts/fetchsra.sh
+++ workflow/scripts/fetchsra.sh
@@ -16,38 +16,19 @@ mem_mb="-m${snakemake_resources[mem_mb]:-2048}M"
 
 reads=(${snakemake_output[reads]})
 read_1=${reads[0]}
-
+read_2=${reads[1]}
 outdir=$(dirname "${read_1}")
 mkdir -p "${outdir}"
 
-# Create temporary directory
-tmpdir=$(mktemp -d -p ${outdir})
-trap 'rm -rf "$tmpdir"' EXIT
-echo ${tmpdir}
-# Ensure output directory exists
+
 
 
 
 if [[ -f $fq1 ]];
 then
-    ln -s $fq1 ${reads[0]}
+    ln -s $fq1 ${read_1}
     if [[ -f $fq2 ]];
     then
-        ln -s $fq2 ${reads[1]}
+        ln -s $fq2 ${read_2}
     fi
-else
-    echo "Downloading data from SRA"
-    echo `date`
-    (fasterq-dump --skip-technical \
-        --temp ${tmpdir} \
-        --threads ${threads} \
-        --mem "${mem_mb}" \
-        --outdir ${outdir} \
-        ${sra_or_fastq}
-        
-        # Compress output files if they exist
-        for file in "$outdir"/*.fastq; do
-            if [ -f "$file" ]; then
-                pigz -p "$threads" "$file"
-            fi
-    done) 2>&1 | tee "$log"
+fi
