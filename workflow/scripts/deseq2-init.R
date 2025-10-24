6ef3bbe11471f7eb7d22716f6d657bae1be140b4
loganylchen <yuelong.chen.btr@gmail.com>
2025-10-24 05:02:39 +0000

    fix: refactor deseq2 initialization for improved clarity and organization

--- workflow/scripts/deseq2-init.R
+++ workflow/scripts/deseq2-init.R
@@ -2,7 +2,12 @@ log <- file(snakemake@log[[1]], open="wt")
 sink(log)
 sink(log, type="message")
 
-library("DESeq2")
+
+suppressPackageStartupMessages({
+    library(dplyr)
+    library(DESeq2)
+})
+
 
 parallel <- FALSE
 if (snakemake@threads > 1) {
@@ -12,29 +17,52 @@ if (snakemake@threads > 1) {
     parallel <- TRUE
 }
 
-# colData and countData must have the same sample order, but this is ensured
-# by the way we create the count matrix
-cts <- read.table(snakemake@input[["counts"]], header=TRUE, row.names="gene", check.names=FALSE,sep='\t')
-cts <- cts[ , order(names(cts))]
 
-coldata <- read.table(snakemake@params[["samples"]], header=TRUE, row.names="sample_name", check.names=FALSE,sep='\t')
-coldata <- coldata[order(row.names(coldata)), , drop=F]
+project<- snakemake@params[["project"]]
+case_condition<-snakemake@params[["case_condition"]]
+control_condition<-snakemake@params[["control_condition"]]
+discovery_sample_type<-snakemake@params[["discovery_sample_type"]]
+samples<-snakemake@params[["samples"]]
+counts <- snakemake@input[["counts"]]
 
-dds <- DESeqDataSetFromMatrix(countData=cts,
-                              colData=coldata,
-                              design=as.formula(snakemake@params[["model"]]))
 
-# remove uninformative columns
-dds <- dds[ rowSums(counts(dds)) > snakemake@params[["count_threshold"]], ]
-# normalization and preprocessing
-dds <- DESeq(dds, parallel=parallel)
+# output
+discovery_count_rds<-snakemake@output[["discovery_count_rds"]]
+validation_count_rds<-snakemake@output[["validation_count_rds"]]
+discovery_vst_rds<-snakemake@output[["discovery_vst_rds"]]
+validation_vst_rds<-snakemake@output[["validation_vst_rds"]]
+
+
+coldata <- read.table(samples, header=TRUE, row.names="sample_name", check.names=FALSE,sep='\t')
+coldata_discovery <- coldata %>% 
+                    dplyr::filter(sample_type==discovery_sample_type)
+
+coldata_validation <- coldata %>% 
+                    dplyr::filter(sample_type != discovery_sample_type)
+
+cts <- read.table(counts, header=TRUE, row.names="Geneid", check.names=FALSE,sep='\t')
+cts_discovery <- cts[,rownames(coldata_discovery)]
+cts_validation <- cts[,rownames(coldata_validation)]
+
+
+dds_discovery <- DESeqDataSetFromMatrix(countData=cts_discovery,
+                              colData=coldata_discovery,
+                              design=~condition)
+dds_validation <- DESeqDataSetFromMatrix(countData=cts_validation,
+                              colData=coldata_validation,
+                              design=~condition)
+
+
+dds_discovery <- DESeq(dds_discovery, parallel=parallel)
+dds_validation <- DESeq(dds_validation, parallel=parallel)
 
 # Write dds object as RDS
-saveRDS(dds, file=snakemake@output[[1]])
-# Write normalized counts
-norm_counts = counts(dds, normalized=T)
-write.table(data.frame("gene"=rownames(norm_counts), norm_counts), file=snakemake@output[[2]], sep='\t', row.names=F,quote=FALSE)
+saveRDS(dds_discovery, file=discovery_count_rds)
+saveRDS(dds_validation, file=validation_count_rds)
 
-vsd <- vst(dds)
+vsd_discovery <- vst(dds_discovery)
+vsd_validation <- vst(dds_validation)
 
-write.table(data.frame("gene"=rownames(norm_counts), assay(vsd)),file=snakemake@output[[3]],sep='\t',row.names=F,quote=FALSE)
+
+saveRDS(vsd_discovery, file=discovery_vst_rds)
+saveRDS(vsd_validation, file=validation_vst_rds)
