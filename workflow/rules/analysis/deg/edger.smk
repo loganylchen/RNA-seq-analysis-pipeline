bf723dff2991b10f5481a1da5caf421328e1e819
loganylchen <yuelong.chen.btr@gmail.com>
2025-12-31 15:24:23 +0800

    Add edgeR rule and script for differential expression analysis

---
+++ workflow/rules/analysis/deg/edger.smk
@@ -0,0 +1,34 @@
+# tool could be STAR_FC or salmon or kallisto
+rule deseq2:
+    input:
+        counts="{project}/quantification/{tool}/count_matrix.txt",
+    output:
+        discovery_count_rds="{project}/DEG/edger/{tool}/discovery_count_matrix.rds",
+        validation_count_rds="{project}/DEG/edger/{tool}/validation_count_matrix.rds",
+        discovery_vst_rds="{project}/DEG/edger/{tool}/discovery_vst_matrix.rds",
+        validation_vst_rds="{project}/DEG/edger/{tool}/validation_vst_matrix.rds",
+        discovery_deg_rds="{project}/DEG/edger/{tool}/discovery_deg.rds",
+        validation_deg_rds="{project}/DEG/edger/{tool}/validation_deg.rds",
+        discovery_deg_tsv="{project}/DEG/edger/{tool}/discovery_deg.tsv",
+        validation_deg_tsv="{project}/DEG/edger/{tool}/validation_deg.tsv",
+    params:
+        samples=config["samples"],
+        project=project,
+        case_condition=case_condition,
+        control_condition=control_condition,
+        discovery_sample_type=discovery_sample_type,
+    container:
+        (
+            "docker://btrspg/edger:4.4.0"
+            if config["container"].get("edger", None) is None
+            else config["container"].get("edger", None)
+        )
+    log:
+        "logs/{project}/edger_{tool}.log",
+    threads: config["threads"].get("edger", 4)
+    benchmark:
+        "benchmarks/{project}/edger_{tool}.benchmark.txt"
+    resources:
+        mem_mb=config["resources"]["mem_mb"].get("edger", 8192),
+    script:
+        "../../scripts/analysis/deg/edger.R"
---
+++ workflow/scripts/analysis/deg/edger.R
@@ -0,0 +1,84 @@
+#!/usr/bin/env Rscript
+# Differential expression analysis using edgeR
+
+# Set up log file sink BEFORE loading libraries
+log_file <- snakemake@log[[1]]
+log <- file(log_file, open = "wt")
+sink(log)
+sink(log, type = "message")
+
+suppressPackageStartupMessages({
+  library(edgeR)
+  library(dplyr)
+})
+
+# Read inputs
+cat("Reading parameters and inputs...\n")
+project<- snakemake@params[["project"]]
+case_condition<-snakemake@params[["case_condition"]]
+control_condition<-snakemake@params[["control_condition"]]
+discovery_sample_type<-snakemake@params[["discovery_sample_type"]]
+samples<-snakemake@params[["samples"]]
+counts <- snakemake@input[["counts"]]
+
+# Read outputs
+
+discovery_deg_rds<-snakemake@output[["discovery_deg_rds"]]
+validation_deg_rds<-snakemake@output[["validation_deg_rds"]]
+discovery_deg_tsv<-snakemake@output[["discovery_deg_tsv"]]
+validation_deg_tsv<-snakemake@output[["validation_deg_tsv"]]
+
+
+cat("=== edgeR Analysis ===\n")
+cat("Preparing coldata...\n")
+coldata <- read.table(samples, header=TRUE, row.names="sample_name", check.names=FALSE,sep='\t',)
+coldata_discovery <- coldata %>% 
+                    dplyr::filter(sample_type==discovery_sample_type)
+coldata_validation <- coldata %>% 
+                    dplyr::filter(sample_type != discovery_sample_type)
+
+cts <- read.table(counts, header=TRUE, row.names="Geneid", check.names=FALSE,sep='\t')
+
+edger_pipeline <- function(count,coldata,
+                            case_condition, 
+                            control_condition, parallel=TRUE) {
+    group <- coldata %>% mutate(condition = as.factor(condition,levels=c(case_condition,control_condition)))[['condition']]
+    cts <- count[,rownames(coldata)]
+    y <- DGEList(counts = cts, 
+                group = group)
+    keep <- filterByExpr(y)
+    y <- y[keep, , keep.lib.sizes = FALSE]
+    y <- calcNormFactors(y)
+    design <- model.matrix(~ group)
+    y <- estimateDisp(y, design)
+    fit <- glmQLFit(y, design)
+    qlf <- glmQLFTest(fit, coef = 2)
+    res <- topTags(qlf, n = Inf, adjust.method = "BH")
+    res_df <- as.data.frame(res)
+    res_df$gene_id <- rownames(res_df)
+    res_df <- res_df[, c("gene_id", "logFC", "logCPM", "F", "PValue", "FDR")]
+    colnames(res_df) <- c("gene_id", "log2FoldChange", "logCPM", "F_statistic", "pvalue", "padj")
+    res_df <- res_df[order(res_df$pvalue), ]
+    return(res_df)
+}
+
+cat("Processing discovery set...\n")
+res_discovery <- edger_pipeline(cts, coldata_discovery,
+                                case_condition,
+                                control_condition)
+saveRDS(res_discovery, discovery_deg_rds)
+write.table(res_discovery, discovery_deg_tsv, sep='\t', quote=FALSE, row.names=FALSE)
+
+cat("Processing validation set...\n")
+res_validation <- edger_pipeline(cts, coldata_validation,
+                                case_condition,
+                                control_condition)
+saveRDS(res_validation, validation_deg_rds)
+write.table(res_validation, validation_deg_tsv, sep='\t', quote=FALSE, row.names=FALSE)
+
+
+
+# Close sink connections
+sink()
+sink(type = "message")
+close(log)
