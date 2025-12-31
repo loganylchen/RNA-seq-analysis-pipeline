642018392663c1b7519a28338069a40f8e7d8105
loganylchen <yuelong.chen.btr@gmail.com>
2025-12-31 12:13:22 +0800

    Change output method for significant results from cat to print for better readability

--- workflow/scripts/modtect_statistical_analysis.R
+++ workflow/scripts/modtect_statistical_analysis.R
@@ -356,7 +356,7 @@ summary_text <- c(
                 `log2FC` = round(log2fc, 3),
                 `P-value` = formatC(p_value, format = "e", digits = 2),
                 FDR = formatC(padj, format = "e", digits = 2)) %>%
-      as.data.frame() %>% cat(.)
+      as.data.frame() %>% print()
   } else {
     "No significant sites found"
   }
