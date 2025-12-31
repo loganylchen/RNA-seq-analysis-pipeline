1e86a9254202683ec65a0b58bd78f928228d1f72
loganylchen <yuelong.chen.btr@gmail.com>
2025-12-31 13:48:09 +0800

    Fix variable name for temporary data in tissue expression statistics

--- workflow/scripts/dcb_analysis.R
+++ workflow/scripts/dcb_analysis.R
@@ -155,7 +155,7 @@ cat("Calculating tissue expression statistics...\n")
 cat(tissue_tumor_mean_tpm[1:5], "\n")
 cat(tissue_normal_mean_tpm[1:5], "\n")
 cat('--------------------------------\n')
-print(head(tmp_data))
+print(head(tpm_data))
 print(gene_col)
 # Identify tissue-upregulated genes (candidate DCBs)
 discovery_genes <- tpm_data %>%
