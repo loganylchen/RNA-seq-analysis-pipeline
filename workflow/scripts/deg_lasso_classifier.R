118a31fd26c41fa271cb6e786babff08bf182b8c
loganylchen <yuelong.chen.btr@gmail.com>
2025-12-31 12:22:47 +0800

    Fix comment character handling in sample info loading and update filtered discovery samples output

--- workflow/scripts/deg_lasso_classifier.R
+++ workflow/scripts/deg_lasso_classifier.R
@@ -54,7 +54,7 @@ discovery_deg <- read.csv(discovery_deg_tsv,sep='\t',check.names=FALSE, row.name
 # Load TPM matrices
 expression_tpm <- read_tsv(expression_tpm_file, show_col_types = FALSE) %>%
                   column_to_rownames(var = colnames(.)[1])
-sample_info <- read_tsv(samples_file, show_col_types = FALSE,comment='#') %>%
+sample_info <- read_tsv(samples_file, show_col_types = FALSE,comment="#") %>%
   filter(project == !!project) %>%
   column_to_rownames(var = "sample_name")
 
@@ -146,7 +146,7 @@ cat("Using", length(feature_genes), "genes for classification\n")
 
 # Prepare discovery data (already computed above)
 cat("\n--- Preparing discovery dataset ---\n")
-cat("Discovery samples (filtered):", length(discovery_samples_filtered), "\n")
+cat("Discovery samples (filtered):", length(discovery_samples), "\n")
 
 # Create design matrix and response
 # Use log10(TPM + 1) for better numerical stability
