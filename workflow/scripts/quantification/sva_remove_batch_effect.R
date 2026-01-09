9f954545e37f1c1b4db2c441733df2fdfa83766f
loganylchen <yuelong.chen.btr@gmail.com>
2026-01-09 13:11:41 +0800

    Enhance logging for batch effect removal process in SVA script

--- workflow/scripts/quantification/sva_remove_batch_effect.R
+++ workflow/scripts/quantification/sva_remove_batch_effect.R
@@ -15,10 +15,18 @@ batch_vars <- unname(unlist(snakemake@params[["batch_vars"]]))
 counts <- snakemake@input[["counts"]]
 coldata_file <- snakemake@input[["coldata"]]
 output_counts <- snakemake@output[["counts"]]
+# more logging
+cat("Starting batch effect removal using ComBat-seq...\n")
+cat(paste0("Batch variables: ", paste(batch_vars, collapse=", "), "\n"))
 
-
-count_matrix <-read.table(counts, header=TRUE, row.names=TRUE, check.names=FALSE,sep='\t')
-coldata <- read.table(coldata_file, header=TRUE, row.names=TRUE, check.names=FALSE,sep='\t')
+cat("Reading count matrix and coldata...\n")
+count_matrix <-read.table(counts, header=TRUE,  check.names=FALSE,sep='\t')
+cat("Count matrix dimensions: ", dim(count_matrix)[1], " genes and ", dim(count_matrix)[2], " samples.\n")
+cat("First few rows of count matrix:\n")
+print(head(count_matrix))
+cat("Preparing batch information...\n")
+coldata <- read.table(coldata_file, header=TRUE, check.names=FALSE,sep='\t')
+cat("Removing batch effects using ComBat-seq...\n")
 batch_info <- as.matrix(coldata[, batch_vars, drop=FALSE])
 
 if(length(batch_vars) == 0) {
